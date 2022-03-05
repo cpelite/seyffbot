40b8fa5822d78fa7d5c89339f6b853f67945a3bb
Benedikt Fellner <bene@fell-it.at>
2022-03-05 14:16:22 +0100

    fixed a multitude of errors (i really need to code more often...)

--- seyffbot.py
+++ seyffbot.py
@@ -7,15 +7,29 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-#Bot-Settings
-bot = discord.bot()
+#intents festlegen
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='$', intents=intents)
 
+#load extensions
+bot.load_extension("commands")
+bot.load_extension("gifs")
+
+#bot-version
+@bot.command()
+async def botinfo(ctx):
+    embed=discord.Embed(title="Informationen über den Bot.")
+    embed.add_field(name="Bot-Version", value="Beta 1 - Albert I.")
+    embed.add_field(name="Entwickler", value="Ihr kennt den verrückten doch.")
+    await ctx.send(embed=embed)
+
+#falls command nicht existiert.
 @bot.event
-async def on_ready():
-    print(f"We have logged in as {bot.user}")
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        embed = discord.Embed(title="Sorry!")
+        embed.add_field(name="Es tut mir leid..", value = "...aber dieser Befehl existiert noch nicht.")
+        await ctx.send(embed=embed)
 
-@bot.slash_command(guild_ids=[931187178980126780])
-async def hello(ctx):
-    await ctx.respond("Hello!")
-
-bot.run("TOKEN")
+bot.run(TOKEN)
